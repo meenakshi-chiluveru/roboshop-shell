f0b0e9dac6b34ab3aa1a8fc5737e2a70b8a8300a
meenakshi <meenakshichiluveru249@gmail.com>
2024-11-07 18:26:03 +0530

    user setup

--- redis.sh
+++ redis.sh
@@ -1,6 +1,8 @@
 dnf module disable redis -y
 dnf module enable redis:7 -y
 dnf install redis -y
+sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 #update redis config file
 systemctl enable redis
 systemctl start redis
