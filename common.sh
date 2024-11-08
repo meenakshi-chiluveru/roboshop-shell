1f4d04f7b5926a896756ac443635eef0208b7421
meenakshi <meenakshichiluveru249@gmail.com>
2024-11-08 15:50:58 +0530

    declaring a variables

--- common.sh
+++ common.sh
@@ -1,18 +1,46 @@
+LOG_FILE=/tmp/roboshop.log
 nodejs(){
-  dnf module disable nodejs -y
-  dnf module enable nodejs:20 -y
-  dnf install nodejs -y
-  cp ${component}.service /etc/systemd/system/${component}.service
-  cp mongo.repo /etc/yum.repos.d/mongo.repo
-  useradd roboshop
-  rm -rf /app
-  mkdir /app
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
-  cd /app
-  unzip /tmp/${component}.zip
-  cd /app
-  npm install
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+  echo disable nodejs default cersion
+  dnf module disable nodejs -y  >$LOG_FILE
+
+  echo enable nodejs version
+  dnf module enable nodejs:20 -y  >$LOG_FILE
+
+  echo install nodejs
+  dnf install nodejs -y  >$LOG_FILE
+
+  echo copy service file
+  cp ${component}.service /etc/systemd/system/${component}.service  >$LOG_FILE
+
+  echo copy mongodb repo file
+  cp mongo.repo /etc/yum.repos.d/mongo.repo  >$LOG_FILE
+
+  echo add user
+  useradd roboshop  >$LOG_FILE
+
+  echo remove/cleqnaing app directory
+  rm -rf /app  >$LOG_FILE
+
+  echo make /app directory
+  mkdir /app  >$LOG_FILE
+
+  echo download application content
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip  >$LOG_FILE
+
+  echo change to app directory
+  cd /app  >$LOG_FILE
+
+  echo extracting application
+  unzip /tmp/${component}.zip  >$LOG_FILE
+
+  echo change directory to /app
+  cd /app  >$LOG_FILE
+
+  echo download nodejs dependencies
+  npm install >$LOG_FILE
+
+  echo start service
+  systemctl daemon-reload >$LOG_FILE
+  systemctl enable ${component} >$LOG_FILE
+  systemctl restart ${component} >$LOG_FILE
 }
