573a7ccfd59e13b1c8b32186772ef930081f0d7d
meenakshi <meenakshichiluveru249@gmail.com>
2024-11-08 13:09:17 +0530

    use functions to keep code DRY

--- cart.sh
+++ cart.sh
@@ -1,15 +1,5 @@
 # loaded source file which is common.sh to cart.sh
 source common.sh
-cp cart.service /etc/systemd/system/cart.service
+component=cart
+# variables concept used here
 nodejs
-useradd roboshop
-rm -rf /App
-/mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cd /app
-unzip /tmp/cart.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart cart
--- user.sh
+++ user.sh
@@ -1,15 +1,4 @@
 # loaded source file which is common.sh to user.sh
 source common.sh
-cp user.service /etc/systemd/system/user.service
+component=user
 nodejs
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
